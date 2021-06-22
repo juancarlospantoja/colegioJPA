35a4ee4d8cc000272b47c1494d30390d6e45a002
Juan <juancarlospantoja@hotmail.com>
2021-06-22 18:23:16 -0500

    refactoring carge de datos

---
+++ src/main/resources/data.sql
@@ -0,0 +1,78 @@
+insert into colegio values(default,'El colegio del Olimpo');
+insert into curso values(default,10,'A',1);
+insert into curso values(default,10,'B',1);
+insert into curso values(default,11,'A',1);
+insert into curso values(default,11,'B',1);
+insert into profesor values(default,'Némesis');
+insert into profesor values(default,'Príapo');
+insert into profesor values(default,'Iris');
+
+insert into estudiante values(default,'Afrodita');
+insert into estudiante values(default,'Apolo');
+insert into estudiante values(default,'Ares');
+insert into estudiante values(default,'Artemisa');
+insert into estudiante values(default,'Atenea');
+insert into estudiante values(default,'Dionisio');
+insert into estudiante values(default,'Hefesto');
+insert into estudiante values(default,'Hera');
+insert into estudiante values(default,'Hermes');
+insert into estudiante values(default,'Hades');
+insert into estudiante values(default,'Poseidón');
+insert into estudiante values(default,'Zeus');
+
+insert into asignatura values(default,'Matemáticas',1,1);
+insert into asignatura values(default,'Español',1,2);
+insert into asignatura values(default,'Ingles básico',1,3);
+
+insert into asignatura values(default,'Matemáticas',2,1);
+insert into asignatura values(default,'Español',2,2);
+insert into asignatura values(default,'Ingles avanzado',2,3);
+
+insert into asignatura values(default,'Matemáticas',3,1);
+insert into asignatura values(default,'Pre Icfes',3,1);
+
+insert into asignatura values(default,'Matemáticas',4,1);
+insert into asignatura values(default,'Pre Icfes',4,1);
+
+insert into asignatura_estudiante values(1,1);
+insert into asignatura_estudiante values(2,1);
+insert into asignatura_estudiante values(3,1);
+
+insert into asignatura_estudiante values(1,2);
+insert into asignatura_estudiante values(2,2);
+insert into asignatura_estudiante values(3,2);
+
+insert into asignatura_estudiante values(1,3);
+insert into asignatura_estudiante values(2,3);
+insert into asignatura_estudiante values(3,3);
+
+insert into asignatura_estudiante values(4,4);
+insert into asignatura_estudiante values(5,4);
+insert into asignatura_estudiante values(6,4);
+
+insert into asignatura_estudiante values(4,5);
+insert into asignatura_estudiante values(5,5);
+insert into asignatura_estudiante values(6,5);
+
+insert into asignatura_estudiante values(4,6);
+insert into asignatura_estudiante values(5,6);
+insert into asignatura_estudiante values(6,6);
+
+insert into asignatura_estudiante values(7,7);
+insert into asignatura_estudiante values(8,7);
+
+insert into asignatura_estudiante values(7,8);
+insert into asignatura_estudiante values(8,8);
+
+insert into asignatura_estudiante values(9,9);
+insert into asignatura_estudiante values(10,9);
+
+insert into asignatura_estudiante values(9,10);
+insert into asignatura_estudiante values(10,10);
+
+insert into asignatura_estudiante values(9,11);
+insert into asignatura_estudiante values(10,11);
+
+insert into asignatura_estudiante values(9,12);
+insert into asignatura_estudiante values(10,12);
+
